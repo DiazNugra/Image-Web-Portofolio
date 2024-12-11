3a54ffdd62136721ae68a749a3d59bc0c37c82f3
DiazNugra <mnugrahaofficial@gmail.com>
2024-12-11 11:52:31 +0700

    First Commit Image Galley

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,    
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: 'Montserrat Alternates',
+      secondary : 'Jost'
+    },    
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#292f36',
+          hover: '#343e4a'
+        },
+        secondary: '#4d5053',
+        accent: {
+          DEFAULT: '#cda274',
+          secondary: '#f4f0ec',
+          hover: '#CCB372',
+        },
+      },
+      backgroundImage: {
+        heroMain: 'url(/dist/img/hero-main.png)',
+        heroMobile : 'url(/dist/img/hero-mobile.png)',
+      },
+    },
+  },
+  plugins: [],
+}
+
